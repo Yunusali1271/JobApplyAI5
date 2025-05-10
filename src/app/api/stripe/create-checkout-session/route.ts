87efae65a2e4f9650e05515ea6d863c4e79aa1cb
Kashif Hasan <kashif.hasan@agreemedia.com>
2025-05-10 17:40:14 +0800

    working stripe checkout integration and fixed subscription error message

--- src/app/api/stripe/create-checkout-session/route.ts
+++ src/app/api/stripe/create-checkout-session/route.ts
@@ -7,21 +7,22 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 
 export async function POST(req: NextRequest) {
   try {
-    const { lineItems } = await req.json();
+    const { lineItems, email } = await req.json();
 
     if (!lineItems || !Array.isArray(lineItems) || lineItems.length === 0) {
       return NextResponse.json({ error: 'Invalid line items provided' }, { status: 400 });
     }
+
     const session = await stripe.checkout.sessions.create({
+      ui_mode : 'embedded',
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'subscription',
-      success_url: `${req.nextUrl.origin}/manage-subscription?success=true`, // Redirect URL after successful payment
-      cancel_url: `${req.nextUrl.origin}/manage-subscription?canceled=true`, // Redirect URL if payment is canceled
+      customer_email : email,
+      return_url: `${req.nextUrl.origin}/stripe-return`,
     });
-    return NextResponse.json({ url: session.url });
+    return NextResponse.json({ clientSecret: session.client_secret });
   } catch (error: any) {
-    console.error('Error creating checkout session:', error);
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
